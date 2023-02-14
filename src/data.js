7473cca378d9735cdf5ef09b92fa2347296b4a2b
Shameer <shameer66ster@gmail.com>
2023-02-14 22:25:18 +0530

    html to react

---
+++ src/data.js
@@ -0,0 +1,78 @@
+import imga from "./images/tour-1.jpeg";
+import imgb from "./images/tour-2.jpeg";
+import imgc from "./images/tour-3.jpeg";
+import imgd from "./images/tour-4.jpeg";
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+export const tours = [
+  {
+    id: 1,
+    image: imga,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: imgb,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 3,
+    image: imgc,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 4,
+    image: imgd,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+];
